c217f18509200d71c6f90b7877b6f4f4a3de4966
Daulet <dauletakberdiyev@gmail.com>
2023-03-28 12:51:12 +0200

    title pro change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="icon.png">
-        <title><%= htmlWebpackPlugin.options.title %></title>
+        <title>People Pro</title>
 
         <!-- bootstrap 5 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
